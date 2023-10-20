876ca6294ae163625d6b826820d596dff8c8f890
Enzo HERICHARD <enzoherichard@MacBook-Pro-de-Enzo.local>
2023-10-20 14:32:13 +0200

    feat: Insert data in sqlite and postgres

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-
+from sqlalchemy import create_engine
+import pandas as pd
+import psycopg2
+import sqlite3
 
 url_spin= "https://www.spin-off.fr/calendrier_des_series.html?date=2023-10"
 
@@ -33,25 +36,24 @@ def scrapping_series():
             infos.append([name, saison, episode, channel, country, href,date])
     return infos
 
-# infos = scrapping_series()
+infos = scrapping_series()
 
 def save_data_file(infos):
     csv_file = "data/files/episodes.csv"
     with open(csv_file, "w") as file:
-        file.write("Name,Saison,Episode,Date,Channel,Country,Href,Date\n")
+        file.write("Name,Saison,Episode,Channel,Country,Href,Date\n")
         for row in infos:
             row_data = ",".join(row)
             file.write(row_data + "\n")
 
-    print(f"Data has been saved to {csv_file}")
+    print(f"Insertion des données dans le {csv_file}.")
 
-# save_data_file(infos)
+save_data_file(infos)
 
 def read_episodes_csv(file_path):
     data = []
     with open(file_path, 'r') as file:
         lines = file.readlines()
-        headers = lines[0].strip().split(',')
         for line in lines[1:]:
             values = line.strip().split(',')
             name = values[0]
@@ -64,7 +66,56 @@ def read_episodes_csv(file_path):
             data.append((name, saison, episode, date, channel, country, href))
     return data
 
+def get_connection_sqlite():
+    conn = sqlite3.connect("data/databases/database.db")
+    return conn
+
+def insert_episodes(data):
+    conn = get_connection_sqlite()
+
+    cur = conn.cursor()
+    cur.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS episodes(
+            name TEXT,
+            saison INTEGER,
+            episode INTEGER,
+            channel TEXT,
+            country TEXT,
+            href TEXT,
+            date TEXT
+        )
+        '''
+    )
+    conn.commit()
+    cur.executemany(
+        "INSERT INTO episodes (name, saison, episode, channel, country, href, date) VALUES (?,?,?,?,?,?,?)",
+        data,
+    )
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+def select_episodes():
+    conn = get_connection_sqlite()
+    cur = conn.cursor()
+    cur.close()
+    conn.close()
+    
 csv_file = "data/files/episodes.csv"
-episodes_data = read_episodes_csv(csv_file)
-for episode in episodes_data:
-    print(episode)
+
+def insert_data_postgres(csv_file):
+    episodes_data = read_episodes_csv(csv_file)
+    
+    episodes_data = pd.read_csv(csv_file)
+
+    URL_DB = "postgres://course_pyth_5801:Z1cufoxTonxlgtnmHFNb@course-pyth-5801.postgresql.a.osc-fr1.scalingo-dbs.com:32624/course_pyth_5801?sslmode=prefer"
+    URL_DB_SQLACHEMY = f"postgresql+psycopg2{URL_DB[8:]}"
+
+    engine = create_engine(URL_DB_SQLACHEMY, connect_args={"sslmode": "allow"})
+
+    episodes_data.to_sql("episodes", engine, if_exists="replace", index=False)
+
+    engine.dispose()
+
